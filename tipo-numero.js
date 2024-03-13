c939b1126c2aea81dd92799ab143eb6ef3370f5e
MiguelCarachenskiNeto <miguel.carachenski.neto@escola.pr.gov.br>
2024-03-13 12:35:23 -0300

    subir repositorio

---
+++ tipo-numero.js
@@ -0,0 +1,12 @@
+const primeiroNumero=10;
+const segundoNumero=20;
+console.log(primeiroNumero + segundoNumero);
+
+
+const primeiroDecimal=3.14;
+const segundoDecimal=.5;
+console.log(primeiroDecimal / segundoDecimal);
+console.log(primeiroDecimal - segundoDecimal);
+console.log(primeiroDecimal * segundoDecimal);
+
+const divisao=primeiroDecimal/segundoDecimal
---
+++ tipo-texto.js
@@ -0,0 +1,7 @@
+const nomeAluno="Beatriz ";
+const idadeAluno=12;
+const maeAluno="Juliana";
+
+console.log(nomeAluno);
+console.log(nomeAluno * idadeAluno)
+console.log(nomeAluno + maeAluno)
